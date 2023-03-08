90d9afb43abf8a99b2479c02547b0ab4ec64b768
Gill Erick <ogayogill95@gmail.com>
2023-03-08 15:53:47 +0300

    Set up project

---
+++ src/main/java/com/example/simplebankapi/SimpleBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplebankapi/SimpleBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
